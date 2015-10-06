9032e7020eac0e33eca0800b6b36f076cf034e99
MB Burch <marybethburch@gmail.com>
2015-10-06 10:06:29 -0600

    Finish login/logout

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,6 +8,10 @@ class UserTest < ActiveSupport::TestCase
                      password_confirmation: "foobar")
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
   test "should be valid" do
     assert @user.valid?
   end
